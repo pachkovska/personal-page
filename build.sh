71e59d371b36b5058192a2124b6edd2537458380
pachkovska <pachkovska@gmail.com>
2019-11-04 17:54:36 -0800

    modified build.sh script

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 cat ./templates/top.html ./content/index.html ./templates/bottom.html > ./docs/index.html
-cat ./templates/top/html ./content/projects.html ./templates/bottom.html > ./docs/projects.html
+cat ./templates/top.html ./content/projects.html ./templates/bottom.html > ./docs/projects.html
 cat ./templates/top.html ./content/resume.html ./templates/bottom.html > ./docs/resume.html
-cat ./templates/top/html ./content/blog.html ./templates/bottom.html > ./docs/blog.html
+cat ./templates/top.html ./content/blog.html ./templates/bottom.html > ./docs/blog.html
 cat ./templates/top.html ./content/contact.html ./templates/bottom.html > ./docs/contact.html
-
-open ./docs/index.html
